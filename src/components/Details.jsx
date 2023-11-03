efe3c66793c3c2046bf19c8411aaf6c9b88d169c
Nicole Moncrieffe <nicoletmoncrieffe@gmail.com>
2023-11-03 14:29:40 +0000

    moved images folder into public

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -11,13 +11,13 @@ function Details(){
 
       <div className='instructions'>
         <div>
-          <img src="images/present.png" alt="present-image" />
+          <img src="./images/present.png" alt="present-image" />
           Welcome to Count Me In, your go-to countdown clock for staying organised and excited about your upcoming special occasions, vacations, and events!         </div>
         <div>
-        <img src="images/calendar.png" alt="calendar-image" />
+        <img src="./images/calendar.png" alt="calendar-image" />
         Just input your event date, select its category, and add any personal notes you'd like. </div>
         <div>
-        <img src="images/celebrate.png" alt="confetti-png" />
+        <img src="./images/celebrate.png" alt="confetti-png" />
           With a single click, your event countdown begins!
         </div>
       </div>
